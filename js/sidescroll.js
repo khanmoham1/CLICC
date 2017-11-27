326c13803bae0a307f908f64721a51d82a2d7a59
khanmoham1 <33469651+khanmoham1@users.noreply.github.com>
2017-11-27 09:56:48 -0500

    Attempted Sidescroll

---
+++ js/sidescroll.js
@@ -0,0 +1,7 @@
+<script src="js/jquery.js"></script>
+<script>
+  $(function() {
+    $("#sidescroll-wrap").wrapInner("<table cellspacing='30'><tr>");
+    $(".book_popular").wrap("<td>");
+  });
+</script>
